048f1dfc40273744cb11ca4d989ae8fa213d6981
Vinicius Vaccari Hehn <vivaccar@c2r2s4.42porto.com>
2024-10-21 16:18:09 +0100

    ex03

---
+++ cpp_05/ex03/srcs/main.cpp
@@ -0,0 +1,9 @@
+#include "../includes/ShrubberyCreationForm.hpp"
+#include "../includes/RobotomyRequestForm.hpp"
+#include "../includes/PresidentialPardonForm.hpp"
+#include "Intern.hpp"
+
+int main(void)
+{
+    
+}

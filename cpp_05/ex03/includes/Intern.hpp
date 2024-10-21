853b7dede5601fe48fab7375bb1c1a6f5edb9c8a
Vinicius Vaccari Hehn <vivaccar@c2r2s4.42porto.com>
2024-10-21 17:09:22 +0100

    doing makeForm

--- cpp_05/ex03/includes/Intern.hpp
+++ cpp_05/ex03/includes/Intern.hpp
@@ -6,6 +6,6 @@ class Intern {
         Intern(const Intern &other);
         ~Intern();
 
-        Intern &operator=(const Intern &other);
-        AForm  *makeForm(const std::string &formName, const std::string &target);
+        Intern  &operator=(const Intern &other);
+        AForm   *makeForm(const std::string &formName, const std::string &target);
 };

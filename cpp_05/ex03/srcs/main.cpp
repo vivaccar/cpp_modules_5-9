2d36a59275c4d46ea626ab54c786ac47016b31df
Vinicius Vaccari <viniciusvaccarih@hotmail.com>
2024-10-26 16:24:37 +0100

    done

--- cpp_05/ex03/srcs/main.cpp
+++ cpp_05/ex03/srcs/main.cpp
@@ -1,9 +1,36 @@
 #include "../includes/ShrubberyCreationForm.hpp"
 #include "../includes/RobotomyRequestForm.hpp"
 #include "../includes/PresidentialPardonForm.hpp"
-#include "Intern.hpp"
+#include "../includes/Intern.hpp"
 
 int main(void)
 {
-    
+    {
+        try
+        {
+            std::cout << GREEN << "------ TESTS FOR DIFFERENT FORMS CREATION --------\n" << RESET << std::endl;
+            Intern  estagiario;
+            AForm   *cur;
+
+            cur = estagiario.makeForm("Anyone", "Manager");
+            if (cur)
+                delete cur;
+            cur = estagiario.makeForm("Robotomy", "Manager");
+            if (cur)
+                delete cur;
+            cur = estagiario.makeForm("shrubbery creation", "Director");
+            if (cur)
+                delete cur;
+            cur = estagiario.makeForm("robotomy request", "Manager");
+            if (cur)
+                delete cur;
+            cur = estagiario.makeForm("presidential pardon", "Vice president");
+            if (cur)
+                delete cur;
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+    }
 }
